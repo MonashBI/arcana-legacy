a39b7021e09cd27afdf0d23d05a3e7944dbce825
Tom Close <tclose@oist.jp>
2016-04-29 16:53:35 +1000

    creating archive unittests

--- test/archive/test_daris.py
+++ test/archive/test_daris.py
@@ -19,10 +19,6 @@ SUBJECT_ID = 12
 STUDY_ID = 1
 TEST_IMAGE = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '_data', 'test_upload.nii.gz'))
-CACHE_DIR = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '_data', 'cache_dir'))
-WORKFLOW_DIR = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '_data', 'workflow_dir'))
 
 
 class TestDarisSession(TestCase):
@@ -223,97 +219,111 @@ class TestDarisToken(TestCase):
 #             self.assertTrue(len(daris.list_projects))
 
 
-class TestDarisSinkAndSource(TestCase):
+class TestDarisArchive(TestCase):
 
-    def test_daris_roundtrip(self):
+    CACHE_DIR = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '_data', 'cache_dir'))
+    WORKFLOW_DIR = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '_data', 'workflow_dir'))
+
+    def setUp(self):
         # Create test data on DaRIS
-        daris = DarisSession(user='test123', password='GaryEgan1',
-                             domain='mon-daris', server=SERVER)
-        with daris:  # Opens the daris session
-            study_id = daris.add_study(
+        self._study_id = None
+        self.daris = DarisSession(user='test123', password='GaryEgan1',
+                                  domain='mon-daris', server=SERVER)
+        # Make cache and working dirs
+        shutil.rmtree(self.CACHE_DIR, ignore_errors=True)
+        shutil.rmtree(self.WORKFLOW_DIR, ignore_errors=True)
+        os.makedirs(self.CACHE_DIR)
+        os.makedirs(self.WORKFLOW_DIR)
+        # Upload test study
+        with self.daris:  # Opens the daris session
+            self.study_id = self.daris.add_study(
                 project_id=PROJECT_ID, subject_id=SUBJECT_ID,
                 processed=False, name='source-sink-unittest-study',
                 description="Used in DarisSource/Sink unittest")
             for name in ('source1', 'source2', 'source3', 'source4'):
-                file_id = daris.add_file(
+                file_id = self.daris.add_file(
                     project_id=PROJECT_ID, subject_id=SUBJECT_ID,
-                    study_id=study_id, processed=False,
+                    study_id=self.study_id, processed=False,
                     name=name, description=(
                         "A file added for DarisSink/Source unittest"))
-                daris.upload(TEST_IMAGE, project_id=PROJECT_ID,
-                             subject_id=SUBJECT_ID, study_id=study_id,
-                             processed=False, file_id=file_id)
-        try:
-            # Create working dirs
-            rmtree_ignore_missing(CACHE_DIR)
-            rmtree_ignore_missing(WORKFLOW_DIR)
-            os.makedirs(CACHE_DIR)
-            os.makedirs(WORKFLOW_DIR)
-            # Create DarisSource node
-            source = pe.Node(DarisSource(), 'source')
-            source.inputs.project_id = PROJECT_ID
-            source.inputs.subject_id = SUBJECT_ID
-            source.inputs.study_id = study_id
-            source.inputs.server = SERVER
-            source.inputs.repo_id = REPO_ID
-            source.inputs.cache_dir = CACHE_DIR
-            source.inputs.domain = 'mon-daris'
-            source.inputs.user = 'test123'
-            source.inputs.password = 'GaryEgan1'
-            source.inputs.files = [
-                ('source1', False), ('source2', False), ('source3', False),
-                ('source4', False)]
-            # Create DataSink node
-            sink = pe.Node(DarisSink(), 'sink')
-            sink.inputs.name = 'unittest_study'
-            sink.inputs.description = (
-                "A study created by the soure-sink unittest")
-            sink.inputs.project_id = PROJECT_ID
-            sink.inputs.subject_id = SUBJECT_ID
-            sink.inputs.study_id = study_id
-            sink.inputs.server = SERVER
-            sink.inputs.repo_id = REPO_ID
-            sink.inputs.cache_dir = CACHE_DIR
-            sink.inputs.domain = 'mon-daris'
-            sink.inputs.user = 'test123'
-            sink.inputs.password = 'GaryEgan1'
-            # Create workflow connecting them together
-            workflow = pe.Workflow('source-sink-unit-test',
-                                   base_dir=WORKFLOW_DIR)
-            workflow.add_nodes((source, sink))
-            workflow.connect([(source, sink,
-                               (('source1', 'sink1'), ('source3', 'sink3'),
-                                ('source4', 'sink4')))])
-            workflow.run()
-            # Check cache was created properly
-            source_cache_dir = os.path.join(
-                CACHE_DIR, str(REPO_ID), str(PROJECT_ID), str(SUBJECT_ID),
-                '1', str(study_id))
-            sink_cache_dir = os.path.join(
-                CACHE_DIR, str(REPO_ID), str(PROJECT_ID), str(SUBJECT_ID),
-                '2', str(study_id))
-            self.assertEqual(sorted(os.listdir(source_cache_dir)),
-                             ['source1', 'source2', 'source3', 'source4'])
-            self.assertEqual(sorted(os.listdir(sink_cache_dir)),
-                             ['sink1', 'sink3', 'sink4'])
-            with daris:
-                files = daris.get_files(
-                    project_id=PROJECT_ID, subject_id=SUBJECT_ID,
-                    study_id=study_id, processed=True, repo_id=REPO_ID)
-            self.assertEqual(sorted(d.name for d in files.itervalues()),
-                             ['sink1', 'sink3', 'sink4'])
-        finally:
-            # Clean up working dirs
-            shutil.rmtree(CACHE_DIR, ignore_errors=True)
-            shutil.rmtree(WORKFLOW_DIR, ignore_errors=True)
-            # Clean up study created for unit-test
+                self.daris.upload(TEST_IMAGE, project_id=PROJECT_ID,
+                                  subject_id=SUBJECT_ID,
+                                  study_id=self.study_id,
+                                  processed=False, file_id=file_id)
+
+    def tearDown(self):
+        # Clean up working dirs
+        shutil.rmtree(self.CACHE_DIR, ignore_errors=True)
+        shutil.rmtree(self.WORKFLOW_DIR, ignore_errors=True)
+        # Clean up study created for unit-test
+        if self.study_id is not None:
             try:
-                with daris:
-                    daris.delete_study(
+                with self.daris:
+                    self.daris.delete_study(
                         project_id=PROJECT_ID, subject_id=SUBJECT_ID,
-                        processed=False, study_id=study_id)
-                    daris.delete_study(
+                        processed=False, study_id=self.study_id)
+                    self.daris.delete_study(
                         project_id=PROJECT_ID, subject_id=SUBJECT_ID,
-                        processed=True, study_id=study_id)
+                        processed=True, study_id=self.study_id)
             except DarisException:
                 pass
+
+    def test_daris_roundtrip(self):
+
+        # Create working dirs
+        # Create DarisSource node
+        source = pe.Node(DarisSource(), 'source')
+        source.inputs.project_id = PROJECT_ID
+        source.inputs.subject_id = SUBJECT_ID
+        source.inputs.study_id = self.study_id
+        source.inputs.server = SERVER
+        source.inputs.repo_id = REPO_ID
+        source.inputs.cache_dir = self.CACHE_DIR
+        source.inputs.domain = 'mon-daris'
+        source.inputs.user = 'test123'
+        source.inputs.password = 'GaryEgan1'
+        source.inputs.files = [
+            ('source1', False), ('source2', False), ('source3', False),
+            ('source4', False)]
+        # Create DataSink node
+        sink = pe.Node(DarisSink(), 'sink')
+        sink.inputs.name = 'unittest_study'
+        sink.inputs.description = (
+            "A study created by the soure-sink unittest")
+        sink.inputs.project_id = PROJECT_ID
+        sink.inputs.subject_id = SUBJECT_ID
+        sink.inputs.study_id = self.study_id
+        sink.inputs.server = SERVER
+        sink.inputs.repo_id = REPO_ID
+        sink.inputs.cache_dir = self.CACHE_DIR
+        sink.inputs.domain = 'mon-daris'
+        sink.inputs.user = 'test123'
+        sink.inputs.password = 'GaryEgan1'
+        # Create workflow connecting them together
+        workflow = pe.Workflow('source-sink-unit-test',
+                               base_dir=self.WORKFLOW_DIR)
+        workflow.add_nodes((source, sink))
+        workflow.connect([(source, sink,
+                           (('source1', 'sink1'), ('source3', 'sink3'),
+                            ('source4', 'sink4')))])
+        workflow.run()
+        # Check cache was created properly
+        source_cache_dir = os.path.join(
+            self.CACHE_DIR, str(REPO_ID), str(PROJECT_ID), str(SUBJECT_ID),
+            '1', str(self.study_id))
+        sink_cache_dir = os.path.join(
+            self.CACHE_DIR, str(REPO_ID), str(PROJECT_ID), str(SUBJECT_ID),
+            '2', str(self.study_id))
+        self.assertEqual(sorted(os.listdir(source_cache_dir)),
+                         ['source1', 'source2', 'source3', 'source4'])
+        self.assertEqual(sorted(os.listdir(sink_cache_dir)),
+                         ['sink1', 'sink3', 'sink4'])
+        with self.daris:
+            files = self.daris.get_files(
+                project_id=PROJECT_ID, subject_id=SUBJECT_ID,
+                study_id=self.study_id, processed=True, repo_id=REPO_ID)
+        self.assertEqual(sorted(d.name for d in files.itervalues()),
+                         ['sink1', 'sink3', 'sink4'])
+
