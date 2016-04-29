5349217b52bd952da6a80cc0ec146a29829fa894
Tom Close <tclose@oist.jp>
2016-04-29 11:34:00 +1000

    implemented LocalSink (based on DarisSink)

--- neuroanalysis/archive/local.py
+++ neuroanalysis/archive/local.py
@@ -1,7 +1,16 @@
 import os.path
 from .base import Archive
+import stat
+import shutil
+import logging
 from nipype.pipeline import engine as pe
 from nipype.interfaces.io import DataGrabber, DataSink
+from nipype.interfaces.base import (
+    Directory, DynamicTraitedSpec, traits, TraitedSpec, BaseInterfaceInputSpec,
+    isdefined)
+
+
+logger = logging.getLogger('NeuroAnalysis')
 
 
 class LocalFileSystem(Archive):
@@ -40,3 +49,91 @@ class LocalFileSystem(Archive):
 
     def sessions_with_dataset(self, file_, project_id, sessions=None):
         raise NotImplementedError
+
+
+class LocalSinkInputSpec(DynamicTraitedSpec, BaseInterfaceInputSpec):
+
+    project_id = traits.Int(  # @UndefinedVariable
+        mandatory=True,
+        desc='The project ID')  # @UndefinedVariable @IgnorePep8
+    subject_id = traits.Int(  # @UndefinedVariable
+        mandatory=True,
+        desc="The subject ID")  # @UndefinedVariable @IgnorePep8
+    study_id = traits.Int(mandatory=False,  # @UndefinedVariable @IgnorePep8
+                          desc="The time point or processed data process ID")
+    name = traits.Str(  # @UndefinedVariable @IgnorePep8
+        mandatory=True, desc=("The name of the processed data group, e.g. "
+                              "'tractography'"))
+    description = traits.Str(mandatory=True,  # @UndefinedVariable
+                             desc="Description of the study")
+    base_dir = Directory(
+        exists=True, desc=("Path to the base directory where the files will"
+                           " be cached before uploading"))
+    _outputs = traits.Dict(  # @UndefinedVariable
+        traits.Str,  # @UndefinedVariable
+        value={},
+        usedefault=True)  # @UndefinedVariable @IgnorePep8
+    # TODO: Not implemented yet
+    overwrite = traits.Bool(  # @UndefinedVariable
+        False, mandatory=True, usedefault=True,
+        desc=("Whether or not to overwrite previously created studies of the "
+              "same name"))
+
+    # Copied from the S3DataSink in the nipype.interfaces.io module
+    def __setattr__(self, key, value):
+        if key not in self.copyable_trait_names():
+            if not isdefined(value):
+                super(LocalSinkInputSpec, self).__setattr__(key, value)
+            self._outputs[key] = value
+        else:
+            if key in self._outputs:
+                self._outputs[key] = value
+            super(LocalSinkInputSpec, self).__setattr__(key, value)
+
+
+class LocalSinkOutputSpec(TraitedSpec):
+
+    out_file = traits.Any(desc='datasink output')  # @UndefinedVariable
+
+
+class LocalSink(DataSink):
+
+    input_spec = LocalSinkInputSpec
+    output_spec = LocalSinkOutputSpec
+
+    def _list_outputs(self):
+        """Execute this module.
+        """
+        # Initiate outputs
+        outputs = self.output_spec().get()
+        out_files = []
+        missing_files = []
+        # Get cache dir for study
+        out_dir = os.path.abspath(os.path.join(*(str(d) for d in (
+            self.inputs.base_dir, self.inputs.project_id,
+            self.inputs.subject_id, self.inputs.name))))
+        # Make study cache dir
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir, stat.S_IRWXU | stat.S_IRWXG)
+        # Loop through files connected to the sink and copy them to the
+        # cache directory and upload to daris.
+        for name, filename in self.inputs._outputs.iteritems():
+            src_path = os.path.abspath(filename)
+            if not isdefined(src_path):
+                missing_files.append((name, src_path))
+                continue  # skip the upload for this file
+            # Copy to local cache
+            dst_path = os.path.join(out_dir, name)
+            out_files.append(dst_path)
+            shutil.copyfile(src_path, dst_path)
+        if missing_files:
+            # FIXME: Not sure if this should be an exception or not,
+            #        indicates a problem but stopping now would throw
+            #        away the files that were created
+            logger.warning(
+                "Missing output files '{}' mapped to names '{}' in "
+                "DarisSink".format("', '".join(f for _, f in missing_files),
+                                   "', '".join(n for n, _ in missing_files)))
+        # Return cache file paths
+        outputs['out_file'] = out_files
+        return outputs
