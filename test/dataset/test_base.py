32fdc0968336dee01dbf3468fb2f12b9e2ced818
Tom Close <tclose@oist.jp>
2016-08-12 16:58:11 +1000

    adding test_base unittest

--- test/dataset/test_base.py
+++ test/dataset/test_base.py
@@ -2,6 +2,9 @@ import os.path
 import shutil
 from unittest import TestCase
 from nipype.pipeline import engine as pe
+from nianalysis.base import Scan
+from nianalysis.formats import nifti_gz_format
+from nianalysis.requirements import mrtrix3_req
 from nianalysis.dataset.base import Dataset, Pipeline
 from nianalysis.interfaces.mrtrix import MRConvert
 
@@ -13,53 +16,87 @@ TEST_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '_data',
 class DummyDataset(Dataset):
 
     def pipeline1(self):
-        workflow = pe.Workflow(name="dummy workflow", base_dir=TEST_DIR)
+        pipeline = self._create_pipeline(
+            name='pipeline1',
+            inputs=['start'],
+            outputs=['pipeline1_1', 'pipeline1_2'],
+            description="A dummy pipeline used to test 'run_pipeline' method",
+            options={},
+            requirements=[mrtrix3_req],
+            citations=[],
+            approx_runtime=1)
         mrconvert = pe.Node(MRConvert(), name="convert")
-        workflow.add_nodes(mrconvert)
-        return Pipeline(
-            dataset=self, name='pipeline1', workflow=workflow,
-            inputs=['original'], outputs=['flipped_x', 'gradients'],
-            description="A dummy pipeline used to test 'run_pipeline' method")
+        # Connect inputs
+        pipeline.connect_input('start', mrconvert, 'in_file')
+        # Connect outputs
+        pipeline.connect_output('pipeline1_1', mrconvert, 'out_file')
+        # Check inputs/outputs are connected
+        pipeline.assert_connected()
+        return pipeline
 
     def pipeline2(self):
-        inputs = []
-        outputs = []
-        workflow = pe.Workflow(name="dummy workflow", base_dir=TEST_DIR)
+        pipeline = self._create_pipeline(
+            name='pipeline2',
+            inputs=['start', 'pipeline1_1'],
+            outputs=['pipeline2'],
+            description="A dummy pipeline used to test 'run_pipeline' method",
+            options={},
+            requirements=[mrtrix3_req],
+            citations=[],
+            approx_runtime=1)
         mrconvert = pe.Node(MRConvert(), name="convert")
-        workflow.add_nodes(mrconvert)
-        return Pipeline(
-            self, 'pipeline2', workflow, inputs, outputs, options={},
-            description="A dummy pipeline used to test 'run_pipeline' method")
+        # Connect inputs
+        pipeline.connect_input('dwi_scan', mrconvert, 'in_file')
+        # Connect outputs
+        pipeline.connect_output('dwi_preproc', mrconvert, 'out_file')
+        # Check inputs/outputs are connected
+        pipeline.assert_connected()
+        return pipeline
 
     def pipeline3(self):
-        inputs = []
-        outputs = []
-        workflow = pe.Workflow(name="dummy workflow", base_dir=TEST_DIR)
+        pipeline = self._create_pipeline(
+            name='pipeline3',
+            inputs=['pipeline1_2'],
+            outputs=['pipeline3'],
+            description="A dummy pipeline used to test 'run_pipeline' method",
+            options={},
+            requirements=[mrtrix3_req],
+            citations=[],
+            approx_runtime=1)
         mrconvert = pe.Node(MRConvert(), name="convert")
-        workflow.add_nodes(mrconvert)
-        return Pipeline(
-            self, 'pipeline3', workflow, inputs, outputs, options={},
-            description="A dummy pipeline used to test 'run_pipeline' method")
+        # Connect inputs
+        pipeline.connect_input('dwi_scan', mrconvert, 'in_file')
+        # Connect outputs
+        pipeline.connect_output('dwi_preproc', mrconvert, 'out_file')
+        # Check inputs/outputs are connected
+        pipeline.assert_connected()
+        return pipeline
 
     def pipeline4(self):
-        inputs = []
-        outputs = []
-        workflow = pe.Workflow(name="dummy workflow", base_dir=TEST_DIR)
+        pipeline = self._create_pipeline(
+            name='pipeline4',
+            inputs=['pipeline3'],
+            outputs=['pipeline4'],
+            description="A dummy pipeline used to test 'run_pipeline' method",
+            options={},
+            requirements=[mrtrix3_req],
+            citations=[],
+            approx_runtime=1)
         mrconvert = pe.Node(MRConvert(), name="convert")
-        workflow.add_nodes(mrconvert)
-        return Pipeline(
-            self, 'pipeline4', workflow, inputs, outputs, options={},
-            description="A dummy pipeline used to test 'run_pipeline' method")
+        # Connect inputs
+        pipeline.connect_input('dwi_scan', mrconvert, 'in_file')
+        # Connect outputs
+        pipeline.connect_output('dwi_preproc', mrconvert, 'out_file')
+        # Check inputs/outputs are connected
+        pipeline.assert_connected()
+        return pipeline
 
-    # The list of dataset components that are acquired by the scanner
-    acquired_components = set(
-        'original')
-
-    generated_components = {
-        'flipped_x': pipeline1,
-        'gradients': pipeline1,
-        'flipped_xy': pipeline2,
-        'flipped_xyz': pipeline3}
+    components = [
+        Scan('start', nifti_gz_format),
+        Scan('pipeline1_1', nifti_gz_format, pipeline1),
+        Scan('pipeline1_2', nifti_gz_format, pipeline1),
+        Scan('pipeline2', nifti_gz_format, pipeline2),
+        Scan('pipeline3', nifti_gz_format, pipeline3)]
 
 
 class TestRunPipeline(TestCase):
