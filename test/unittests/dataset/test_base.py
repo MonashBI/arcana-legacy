3392a726511d511e2304028ba5f7c0d1e3b0abd9
Tom Close <tclose@oist.jp>
2016-09-14 14:07:20 +1000

    making base unittests more rigorous

--- test/unittests/dataset/test_base.py
+++ test/unittests/dataset/test_base.py
@@ -1,14 +1,15 @@
 import os.path
 import shutil
 from unittest import TestCase
+import subprocess as sp
 from nipype.pipeline import engine as pe
 from nianalysis.base import Scan
-from nianalysis.formats import nifti_gz_format
+from nianalysis.formats import nifti_gz_format, mrtrix_format
 from nianalysis.requirements import mrtrix3_req
 from nianalysis.dataset.base import Dataset, _create_component_dict
 from nianalysis.interfaces.mrtrix import MRConvert, MRCat, MRMath
 from nianalysis.archive.local import (
-    LocalArchive, LocalSubjectSink, LocalProjectSink, SUBJECT_SUMMARY_NAME,
+    LocalArchive, SUBJECT_SUMMARY_NAME,
     PROJECT_SUMMARY_NAME)
 from nianalysis.testing import test_data_dir
 import logging
@@ -108,7 +109,7 @@ class DummyDataset(Dataset):
     def subject_summary_pipeline(self):
         pipeline = self._create_pipeline(
             name="subject_summary",
-            inputs=['start'],
+            inputs=['ones_slice'],
             outputs=["subject_summary"],
             description=("Test of project summary variables"),
             options={},
@@ -119,7 +120,7 @@ class DummyDataset(Dataset):
                              joinfield=['in_files'], name='mrmath')
         mrmath.inputs.operation = 'sum'
         # Connect inputs
-        pipeline.connect_input('start', mrmath, 'in_files')
+        pipeline.connect_input('ones_slice', mrmath, 'in_files')
         # Connect outputs
         pipeline.connect_output('subject_summary', mrmath, 'out_file')
         pipeline.assert_connected()
@@ -128,7 +129,7 @@ class DummyDataset(Dataset):
     def project_summary_pipeline(self):
         pipeline = self._create_pipeline(
             name="project_summary",
-            inputs=['start'],
+            inputs=['ones_slice'],
             outputs=["project_summary"],
             description=("Test of project summary variables"),
             options={},
@@ -139,7 +140,7 @@ class DummyDataset(Dataset):
                              joinfield=['in_files'], name='mrmath')
         mrmath.inputs.operation = 'sum'
         # Connect inputs
-        pipeline.connect_input('start', mrmath, 'in_files')
+        pipeline.connect_input('ones_slice', mrmath, 'in_files')
         # Connect outputs
         pipeline.connect_output('project_summary', mrmath, 'out_file')
         pipeline.assert_connected()
@@ -147,24 +148,27 @@ class DummyDataset(Dataset):
 
     _components = _create_component_dict(
         Scan('start', nifti_gz_format),
+        Scan('ones_slice', mrtrix_format),
         Scan('pipeline1_1', nifti_gz_format, pipeline1),
         Scan('pipeline1_2', nifti_gz_format, pipeline1),
         Scan('pipeline2', nifti_gz_format, pipeline2),
         Scan('pipeline3', nifti_gz_format, pipeline3),
         Scan('pipeline4', nifti_gz_format, pipeline4),
-        Scan('subject_summary', nifti_gz_format, subject_summary_pipeline,
+        Scan('subject_summary', mrtrix_format, subject_summary_pipeline,
              multiplicity='per_subject'),
-        Scan('project_summary', nifti_gz_format, project_summary_pipeline,
+        Scan('project_summary', mrtrix_format, project_summary_pipeline,
              multiplicity='per_project'))
 
 
 class TestRunPipeline(TestCase):
 
     PROJECT_ID = 'PROJECTID'
-    SUBJECT_IDS = ['SUBJECTID1', 'SUBJECTID2']
+    SUBJECT_IDS = ['SUBJECTID1', 'SUBJECTID2', 'SUBJECTID3']
     SESSION_IDS = ['SESSIONID1', 'SESSIONID2']
     TEST_IMAGE = os.path.abspath(os.path.join(test_data_dir,
                                               'test_image.nii.gz'))
+    ONES_SLICE_IMAGE = os.path.abspath(os.path.join(test_data_dir,
+                                                    'ones_slice.mif'))
     TEST_DIR = os.path.abspath(os.path.join(test_data_dir, 'dataset'))
     BASE_DIR = os.path.abspath(os.path.join(TEST_DIR, 'base_dir'))
     WORKFLOW_DIR = os.path.abspath(os.path.join(TEST_DIR, 'workflow_dir'))
@@ -187,10 +191,13 @@ class TestRunPipeline(TestCase):
                 os.makedirs(session_path)
                 shutil.copy(self.TEST_IMAGE,
                             os.path.join(session_path, 'start.nii.gz'))
+                shutil.copy(self.ONES_SLICE_IMAGE,
+                            os.path.join(session_path, 'ones_slice.mif'))
         archive = LocalArchive(self.BASE_DIR)
         self.dataset = DummyDataset(
             'TestDummy', self.PROJECT_ID, archive,
-            input_scans={'start': Scan('start', nifti_gz_format)})
+            input_scans={'start': Scan('start', nifti_gz_format),
+                         'ones_slice': Scan('ones_slice', mrtrix_format)})
 
     def tearDown(self):
         # Clean up working dirs
@@ -208,14 +215,24 @@ class TestRunPipeline(TestCase):
     def test_subject_summary(self):
         self.dataset.subject_summary_pipeline().run()
         for subject_path in self.subject_paths:
-            self.assertTrue(
-                os.path.exists(os.path.join(
-                    subject_path, SUBJECT_SUMMARY_NAME,
-                    'subject_summary.nii.gz')))
+            summary_path = os.path.join(subject_path, SUBJECT_SUMMARY_NAME,
+                                        'subject_summary.mif')
+            # Get mean value from resultant image (should be the same as the
+            # number of sessions as the original image is full of ones and
+            # all sessions have been summed together
+            mean_val = float(sp.check_output(
+                'mrstats {} -output mean'.format(summary_path), shell=True))
+            self.assertEqual(mean_val, len(self.SESSION_IDS))
 
     def test_project_summary(self):
         self.dataset.project_summary_pipeline().run()
-        self.assertTrue(
-            os.path.exists(os.path.join(
-                self.BASE_DIR, self.PROJECT_ID, PROJECT_SUMMARY_NAME,
-                'project_summary.nii.gz')))
+        summary_path = os.path.join(
+            self.BASE_DIR, self.PROJECT_ID, PROJECT_SUMMARY_NAME,
+            'project_summary.mif')
+        # Get mean value from resultant image (should be the same as the
+        # number of sessions as the original image is full of ones and
+        # all sessions have been summed together
+        mean_val = float(sp.check_output(
+            'mrstats {} -output mean'.format(summary_path), shell=True))
+        self.assertEqual(mean_val,
+                         len(self.SUBJECT_IDS) * len(self.SESSION_IDS))
