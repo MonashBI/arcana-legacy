5115b8ee447d8235259996f387e16bfc0fd45a67
Tom Close <tclose@oist.jp>
2016-12-01 17:47:04 +1100

    fixed up combined study dataset maps and removed extra '.' from MRtrix interfaces

--- nianalysis/study/combined.py
+++ nianalysis/study/combined.py
@@ -35,7 +35,7 @@ class CombinedStudy(Study):
         study and the mapping of their dataset names. The key of the outer
         dictionary will be the name of the sub-study, and the value is a tuple
         consisting of the class of the sub-study and a map of dataset names
-        between the sub-study and the combined study e.g.
+        from the combined study to the sub-study e.g.
 
             sub_study_specs = {'t1_study': (MRStudy, {'t1': 'mr_scan'}),
                                't2_study': (MRStudy, {'t2': 'mr_scan'})}
@@ -46,99 +46,6 @@ class CombinedStudy(Study):
     """
 
     __metaclass__ = ABCMeta
-
-    class TranslatedPipeline(Pipeline):
-        """
-        A pipeline that is translated from a sub-study to the combined study.
-        It takes the untranslated pipeline as an input and destroys it in the
-        process
-
-        Parameters
-        ----------
-        name : str
-            Name of the translated pipeline
-        pipeline : Pipeline
-            Sub-study pipeline to translate
-        combined_study : CombinedStudy
-            Study to translate the pipeline to
-        add_inputs : list[str]
-            List of additional inputs to add to the translated pipeline to be
-            connected manually in combined-study getter (i.e. not using
-            translate_getter decorator).
-        add_outputs : list[str]
-            List of additional outputs to add to the translated pipeline to be
-            connected manually in combined-study getter (i.e. not using
-            translate_getter decorator).
-        """
-
-        def __init__(self, name, pipeline, combined_study, add_inputs=None,
-                     add_outputs=None):
-            self._name = name
-            self._study = combined_study
-            self._workflow = pipeline.workflow
-            ss_name = pipeline.study.name[(len(combined_study.name) + 1):]
-            ss_cls, dataset_map = combined_study.sub_study_specs[ss_name]
-            assert isinstance(pipeline.study, ss_cls)
-            # Translate inputs from sub-study pipeline
-            try:
-                self._inputs = [dataset_map[i] for i in pipeline.inputs]
-            except KeyError as e:
-                raise NiAnalysisMissingDatasetError(
-                    "'{}' input required for pipeline '{}' in '{}' is not "
-                    "present in dataset map:\n{}".format(e, pipeline.name,
-                                                         ss_name, dataset_map))
-            # Add additional inputs
-            self._unconnected_inputs = set()
-            if add_inputs is not None:
-                self._check_spec_names(add_inputs, 'additional inputs')
-                self._inputs.extend(add_inputs)
-                self._unconnected_inputs.update(add_inputs)
-            # Create new input node
-            self._inputnode = pe.Node(IdentityInterface(fields=self._inputs),
-                                      name="{}_inputnode".format(name))
-            # Connect to sub-study input node
-            for inpt in pipeline.inputs:
-                self.workflow.connect(self._inputnode, dataset_map[inpt],
-                                      pipeline.inputnode, inpt)
-            # Translate outputs from sub-study pipeline
-            self._outputs = {}
-            for mult in pipeline.mutliplicities:
-                try:
-                    self._outputs[mult] = [
-                        dataset_map[o]
-                        for o in pipeline.multiplicity_outputs(mult)]
-                except KeyError as e:
-                    raise NiAnalysisMissingDatasetError(
-                        "'{}' output required for pipeline '{}' in '{}' is not"
-                        " present in dataset map:\n{}".format(
-                            e, pipeline.name, ss_name, dataset_map))
-            # Add additional outputs
-            self._unconnected_outputs = set()
-            if add_outputs is not None:
-                self._check_spec_names(add_outputs, 'additional outputs')
-                for output in add_outputs:
-                    combined_study.dataset_spec(output).multiplicity
-                    self._outputs[mult].append(output)
-                self._unconnected_outputs.update(add_outputs)
-            # Create output nodes for each multiplicity
-            self._outputnodes = {}
-            for mult in pipeline.mutliplicities:
-                self._outputnodes[mult] = pe.Node(
-                    IdentityInterface(fields=self._outputs[mult]),
-                    name="{}_{}_outputnode".format(name, mult))
-                # Connect sub-study outputs
-                for output in pipeline.multiplicity_outputs(mult):
-                    self.workflow.connect(pipeline.outputnode(mult), output,
-                                          self._outputnodes[mult],
-                                          dataset_map[output])
-            # Copy additional info fields
-            self._citations = pipeline.citations
-            self._options = pipeline.options
-            self._description = pipeline.description
-            self._requirements = pipeline.requirements
-            self._approx_runtime = pipeline.approx_runtime
-            self._min_nthreads = pipeline.min_nthreads
-            self._max_nthreads = pipeline.max_nthreads
 
     def __init__(self, name, project_id, archive, input_datasets):
         super(CombinedStudy, self).__init__(name, project_id, archive,
@@ -191,3 +98,99 @@ class CombinedStudy(Study):
             trans_pipeline.assert_connected()
             return trans_pipeline
         return translated_getter
+
+    class TranslatedPipeline(Pipeline):
+        """
+        A pipeline that is translated from a sub-study to the combined study.
+        It takes the untranslated pipeline as an input and destroys it in the
+        process
+
+        Parameters
+        ----------
+        name : str
+            Name of the translated pipeline
+        pipeline : Pipeline
+            Sub-study pipeline to translate
+        combined_study : CombinedStudy
+            Study to translate the pipeline to
+        add_inputs : list[str]
+            List of additional inputs to add to the translated pipeline to be
+            connected manually in combined-study getter (i.e. not using
+            translate_getter decorator).
+        add_outputs : list[str]
+            List of additional outputs to add to the translated pipeline to be
+            connected manually in combined-study getter (i.e. not using
+            translate_getter decorator).
+        """
+
+        def __init__(self, name, pipeline, combined_study, add_inputs=None,
+                     add_outputs=None):
+            self._name = name
+            self._study = combined_study
+            self._workflow = pipeline.workflow
+            ss_name = pipeline.study.name[(len(combined_study.name) + 1):]
+            ss_cls, dataset_map = combined_study.sub_study_specs[ss_name]
+            inv_dataset_map = dict(
+                (v, k) for k, v in dataset_map.iteritems())
+            assert isinstance(pipeline.study, ss_cls)
+            # Translate inputs from sub-study pipeline
+            try:
+                self._inputs = [inv_dataset_map[i]
+                                for i in pipeline.inputs]
+            except KeyError as e:
+                raise NiAnalysisMissingDatasetError(
+                    "{} input required for pipeline '{}' in '{}' is not "
+                    "present in dataset map:\n{}".format(e, pipeline.name,
+                                                         ss_name, dataset_map))
+            # Add additional inputs
+            self._unconnected_inputs = set()
+            if add_inputs is not None:
+                self._check_spec_names(add_inputs, 'additional inputs')
+                self._inputs.extend(add_inputs)
+                self._unconnected_inputs.update(add_inputs)
+            # Create new input node
+            self._inputnode = pe.Node(IdentityInterface(fields=self._inputs),
+                                      name="{}_inputnode".format(name))
+            # Connect to sub-study input node
+            for inpt in pipeline.inputs:
+                self.workflow.connect(self._inputnode, inv_dataset_map[inpt],
+                                      pipeline.inputnode, inpt)
+            # Translate outputs from sub-study pipeline
+            self._outputs = {}
+            for mult in pipeline.mutliplicities:
+                try:
+                    self._outputs[mult] = [
+                        inv_dataset_map[o]
+                        for o in pipeline.multiplicity_outputs(mult)]
+                except KeyError as e:
+                    raise NiAnalysisMissingDatasetError(
+                        "'{}' output required for pipeline '{}' in '{}' is not"
+                        " present in inverse dataset map:\n{}".format(
+                            e, pipeline.name, ss_name, inv_dataset_map))
+            # Add additional outputs
+            self._unconnected_outputs = set()
+            if add_outputs is not None:
+                self._check_spec_names(add_outputs, 'additional outputs')
+                for output in add_outputs:
+                    combined_study.dataset_spec(output).multiplicity
+                    self._outputs[mult].append(output)
+                self._unconnected_outputs.update(add_outputs)
+            # Create output nodes for each multiplicity
+            self._outputnodes = {}
+            for mult in pipeline.mutliplicities:
+                self._outputnodes[mult] = pe.Node(
+                    IdentityInterface(fields=self._outputs[mult]),
+                    name="{}_{}_outputnode".format(name, mult))
+                # Connect sub-study outputs
+                for output in pipeline.multiplicity_outputs(mult):
+                    self.workflow.connect(pipeline.outputnode(mult), output,
+                                          self._outputnodes[mult],
+                                          inv_dataset_map[output])
+            # Copy additional info fields
+            self._citations = pipeline.citations
+            self._options = pipeline.options
+            self._description = pipeline.description
+            self._requirements = pipeline.requirements
+            self._approx_runtime = pipeline.approx_runtime
+            self._min_nthreads = pipeline.min_nthreads
+            self._max_nthreads = pipeline.max_nthreads
