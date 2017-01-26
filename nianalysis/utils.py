dd759eefc7af89ea4a3fb5271b3354c88fe86e0b
Tom Close <tclose@oist.jp>
2017-01-27 10:25:25 +1100

    adding coregister to atlas pipelines

--- nianalysis/utils.py
+++ nianalysis/utils.py
@@ -1,6 +1,42 @@
 import os.path
+from nianalysis.exceptions import NiAnalysisError
+
 
 zip_exts = ('gz', 'zip')
+
+package_dir = os.path.join(os.path.dirname(__file__), '..')
+
+
+def get_atlas_path(name, dataset='image'):
+    """
+    Returns the path to the atlas (or atlas mask) in the nianalysis repository
+
+    Parameters
+    ----------
+    name : str
+        Name of the Atlas, can be one of ('mni_nl6')
+    atlas_type : str
+        Whether to return the brain mask or the full atlas, can be one of
+        'image', 'mask'
+    """
+    if name == 'mni_nl6':
+        # MNI ICBM 152 non-linear 6th Generation Symmetric Average Brain
+        # Stereotaxic Registration Model (http://nist.mni.mcgill.ca/?p=858)
+        base_path = os.path.join(package_dir, 'reference', 'atlases',
+                                 'mni_nl6')
+        if dataset == 'image':
+            path = os.path.join(base_path,
+                                'icbm_avg_152_t1_tal_nlin_symmetric_VI.mnc')
+        elif dataset == 'mask':
+            path = os.path.join(
+                base_path, 'icbm_avg_152_t1_tal_nlin_symmetric_VI_mask.mnc')
+        else:
+            raise NiAnalysisError("Unrecognised dataset '{}'"
+                                  .format(dataset))
+    else:
+        raise NiAnalysisError("Unrecognised atlas name '{}'"
+                              .format(name))
+    return path
 
 
 def split_extension(path):
