a21ea657981b186f0af08d5da9034733e6524772
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2017-01-31 13:02:13 +1100

    added fnirt options from FSL config file T1_2_MNI152_2mm

--- nianalysis/utils.py
+++ nianalysis/utils.py
@@ -35,7 +35,11 @@ def get_atlas_path(name, dataset='brain', resolution='1mm'):
                                 'MNI152_T1_{}.nii.gz'.format(resolution))
         elif dataset == 'mask':
             path = os.path.join(get_fsl_reference_path(),
-                                'MNI152_T1_{}_brain_mask.nii.gz'
+                                'MNI152_T1_{}_brain_mask_dil.nii.gz'
+                                .format(resolution))
+        elif dataset == 'masked':
+            path = os.path.join(get_fsl_reference_path(),
+                                'MNI152_T1_{}_brain.nii.gz'
                                 .format(resolution))
         else:
             raise NiAnalysisError("Unrecognised dataset '{}'"
