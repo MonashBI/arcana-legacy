149d490f01af674763ed2693211fb1f92746916c
Tom Close <tom.g.close@gmail.com>
2017-07-10 14:31:04 +1000

    Allow requirements to be a tuple of potential options

--- nianalysis/nodes.py
+++ nianalysis/nodes.py
@@ -5,8 +5,10 @@ import math
 import subprocess as sp
 from collections import defaultdict
 import logging
+from nianalysis.requirements import Requirement
 from nianalysis.exceptions import (
-    NiAnalysisError, NiAnalysisModulesNotInstalledException)
+    NiAnalysisError, NiAnalysisModulesNotInstalledException,
+    NiAnalysisRequirementVersionException)
 from nipype.pipeline.engine import (
     Node as NipypeNode, JoinNode as NipypeJoinNode, MapNode as NipypeMapNode)
 
@@ -72,31 +74,52 @@ class NiAnalysisNodeMixin(object):
             preloaded = self._preloaded_modules()
             logger.debug("Loading required modules {} for '{}'"
                          .format(self._requirements, self.name))
-            for req in self._requirements:
-                try:
-                    version = req.split_version(preloaded[req.name])
-                    logger.debug("Found preloaded version {} of module '{}'"
-                                 .format(version, req.name))
-                    if not req.valid_version(version):
-                        raise NiAnalysisError(
-                            "Incompatible module version already loaded {}/{},"
-                            " (valid {}->{}) please unload before running "
-                            "pipeline"
-                            .format(
-                                req.name, version, req.min_version,
-                                (req.max_version if req.max_version is not None
-                                 else '')))
-                except KeyError:
-                    best_version = req.best_version(
-                        self.available_modules()[req.name])
-                    logger.debug("Loading best version '{}' of module '{}' for"
-                                 " requirement {}".format(best_version,
-                                                          req.name, req))
-                    self.load_module(req.name, best_version)
-                    self._loaded_modules.append((req.name, best_version))
+            for possible_reqs in self._requirements:
+                # If possible reqs is a singleton, wrap it in a list for
+                # iterating
+                if isinstance(possible_reqs, Requirement):
+                    possible_reqs = [possible_reqs]
+                # Loop through all options for a given requirement and see
+                # if at least one can be satisfied.
+                ver_exceptions = []  # Will hold all version error messages
+                for req in possible_reqs:
+                    try:
+                        self._load_requirement(req, preloaded)
+                        break
+                    except NiAnalysisRequirementVersionException as e:
+                        ver_exceptions.append(e)
+                # Check to see if at least one option can be satisfied,
+                # otherwise raise exception with combined messages from all
+                # options.
+                if len(ver_exceptions) == len(possible_reqs):
+                    raise NiAnalysisRequirementVersionException(
+                        ' and '.join(e.msg for e in ver_exceptions))
         except NiAnalysisModulesNotInstalledException as e:
             logger.debug("Skipping loading modules as '{}' is not set"
                          .format(e))
+
+    def _load_requirement(self, req, preloaded):
+        try:
+            version = req.split_version(preloaded[req.name])
+            logger.debug("Found preloaded version {} of module '{}'"
+                         .format(version, req.name))
+            if not req.valid_version(version):
+                raise NiAnalysisError(
+                    "Incompatible module version already loaded {}/{},"
+                    " (valid {}->{}) please unload before running "
+                    "pipeline"
+                    .format(
+                        req.name, version, req.min_version,
+                        (req.max_version if req.max_version is not None
+                         else '')))
+        except KeyError:
+            best_version = req.best_version(
+                self.available_modules()[req.name])
+            logger.debug("Loading best version '{}' of module '{}' for"
+                         " requirement {}".format(best_version,
+                                                  req.name, req))
+            self.load_module(req.name, best_version)
+            self._loaded_modules.append((req.name, best_version))
 
     def _unload_modules(self):
         try:
