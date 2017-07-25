e95ae2b1d5250e7fab7e38251958b6bf67624049
Phillip Ward <phillip.ward@monash.edu>
2017-07-25 18:38:35 +1000

    Removed walltime change

--- nianalysis/pipeline.py
+++ nianalysis/pipeline.py
@@ -412,8 +412,7 @@ class Pipeline(object):
                 self.study._project_id,
                 (self.study.dataset(o) for o in outputs), mult,
                 study_name=self.study.name,
-                name='{}_{}_sink'.format(self.name, mult),
-                memory=16000)
+                name='{}_{}_sink'.format(self.name, mult))
             sink.inputs.description = self.description
             sink.inputs.name = self._study.name
             if mult in ('per_session', 'per_subject'):
