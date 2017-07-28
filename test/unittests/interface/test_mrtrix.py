9ddf2a46e901bd77d5de38cd7bd05d61e5c01f78
Tom Close <tom.g.close@gmail.com>
2017-07-28 14:37:21 +1000

    updated requirements of mrtrix to 3.0rc

--- test/unittests/interface/test_mrtrix.py
+++ test/unittests/interface/test_mrtrix.py
@@ -3,7 +3,7 @@ from nianalysis.testing import BaseTestCase
 import logging
 from nianalysis.nodes import Node
 from nianalysis.interfaces.mrtrix import MRCalc
-from nianalysis.requirements import mrtrix3rc_req
+from nianalysis.requirements import mrtrix3_req
 
 logger = logging.getLogger('NiAnalysis')
 
@@ -13,7 +13,7 @@ class TestMRCalcInterface(BaseTestCase):
     def test_subtract(self):
         # Create Zip node
         mrcalc = Node(MRCalc(), name='mrcalc',
-                      requirements=[mrtrix3rc_req])
+                      requirements=[mrtrix3_req])
         mrcalc.inputs.operands = [os.path.join(self.session_dir, 'threes.mif'),
                                   os.path.join(self.session_dir, 'ones.mif')]
         mrcalc.inputs.operation = 'subtract'
