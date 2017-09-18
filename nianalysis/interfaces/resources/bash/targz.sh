ba4ae9419af4f27cdfd56b8ba3ffe555817566e6
Francesco Sforazzini <francesco.sforazzini@monash.edu>
2017-09-18 15:13:48 +1000

    added file in resource

--- nianalysis/interfaces/resources/bash/targz.sh
+++ nianalysis/interfaces/resources/bash/targz.sh
@@ -2,7 +2,8 @@
 # TAR_GZ a directory relative to its parent (i.e. missing out intermediate directories)
 
 cwd=`pwd`
+dname=`basename $1`
 pushd `dirname $2`
-tar -zcvf $cwd/$1.tar.gz `basename $2`
+tar -zcvf $cwd/$1 `basename $2`
 popd
 
