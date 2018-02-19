8ddaac4214516684c366697a3fd3a0886ec54092
Tom Close <tom.g.close@gmail.com>
2018-02-19 17:05:34 +1100

    check for cached directories in travis build

--- install/dcm2niix.sh
+++ install/dcm2niix.sh
@@ -3,19 +3,23 @@
 
 PKG_DIR=$HOME/packages/dcm2niix
 BUILD_DIR=$PKG_DIR/build
-mkdir -p $BUILD_DIR
 
-git clone https://github.com/rordenlab/dcm2niix.git $PKG_DIR
-cd $BUILD_DIR
-cmake ..
-make
+if [ -d $PKG_DIR ]; then
+  mkdir -p $BUILD_DIR
+  git clone https://github.com/rordenlab/dcm2niix.git $PKG_DIR
+  cd $BUILD_DIR
+  cmake ..
+  make
+fi
 
-mkdir -p $HOME/modules/dcm2niix
-echo '#%Module1.0' >> $HOME/modules/dcm2niix/1.0.2
-echo 'proc ModulesHelp { } {' >> $HOME/modules/dcm2niix/1.0.2
-echo 'global dotversion' >> $HOME/modules/dcm2niix/1.0.2
-echo 'puts stderr "\tDcm2niix"' >> $HOME/modules/dcm2niix/1.0.2
-echo '}' >> $HOME/modules/dcm2niix/1.0.2
-echo 'module-whatis "Dcm2niix"' >> $HOME/modules/dcm2niix/1.0.2
-echo 'conflict dcm2niix' >> $HOME/modules/dcm2niix/1.0.2
-echo "prepend-path PATH $HOME/packages/dcm2niix/build/bin" >> $HOME/modules/dcm2niix/1.0.2
+if [ -d $HOME/modules/dcm2niix ]; then
+  mkdir -p $HOME/modules/dcm2niix
+  echo '#%Module1.0' >> $HOME/modules/dcm2niix/1.0.2
+  echo 'proc ModulesHelp { } {' >> $HOME/modules/dcm2niix/1.0.2
+  echo 'global dotversion' >> $HOME/modules/dcm2niix/1.0.2
+  echo 'puts stderr "\tDcm2niix"' >> $HOME/modules/dcm2niix/1.0.2
+  echo '}' >> $HOME/modules/dcm2niix/1.0.2
+  echo 'module-whatis "Dcm2niix"' >> $HOME/modules/dcm2niix/1.0.2
+  echo 'conflict dcm2niix' >> $HOME/modules/dcm2niix/1.0.2
+  echo "prepend-path PATH $HOME/packages/dcm2niix/build/bin" >> $HOME/modules/dcm2niix/1.0.2
+fi
--- install/mrtrix.sh
+++ install/mrtrix.sh
@@ -2,25 +2,29 @@
 # Clones and builds a copy of dcm2niix
 
 PKG_DIR=$HOME/packages/mrtrix
-mkdir -p $PKG_DIR
 
 # Try to limit the memory required
 export NUMBER_OF_PROCESSORS=4
 
-git clone https://github.com/MRtrix3/mrtrix3.git $PKG_DIR
-cd $PKG_DIR
-./configure
-./build -nowarnings
+if [ -d $PKG_DIR ]; then
+  mkdir -p $PKG_DIR
+  git clone https://github.com/MRtrix3/mrtrix3.git $PKG_DIR
+  cd $PKG_DIR
+  ./configure
+  ./build -nowarnings
+fi
 
 
 # Create modulefile
-mkdir -p $HOME/modules/mrtrix
-echo '#%Module1.0' >> $HOME/modules/mrtrix/3
-echo 'proc ModulesHelp { } {' >> $HOME/modules/mrtrix/3
-echo 'global dotversion' >> $HOME/modules/mrtrix/3
-echo 'puts stderr "\tMRtrix 3"' >> $HOME/modules/mrtrix/3
-echo '}' >> $HOME/modules/mrtrix/3
-echo 'module-whatis "MRtrix 3"' >> $HOME/modules/mrtrix/3
-echo 'conflict mrtrix' >> $HOME/modules/mrtrix/3
-echo "prepend-path PATH $HOME/packages/mrtrix/bin" >> $HOME/modules/mrtrix/3
-echo "prepend-path LD_LIBRARY_PATH $HOME/packages/mrtrix/lib" >> $HOME/modules/mrtrix/3
+if [ -d $HOME/modules/mrtrix ]; then
+  mkdir -p $HOME/modules/mrtrix
+  echo '#%Module1.0' >> $HOME/modules/mrtrix/3
+  echo 'proc ModulesHelp { } {' >> $HOME/modules/mrtrix/3
+  echo 'global dotversion' >> $HOME/modules/mrtrix/3
+  echo 'puts stderr "\tMRtrix 3"' >> $HOME/modules/mrtrix/3
+  echo '}' >> $HOME/modules/mrtrix/3
+  echo 'module-whatis "MRtrix 3"' >> $HOME/modules/mrtrix/3
+  echo 'conflict mrtrix' >> $HOME/modules/mrtrix/3
+  echo "prepend-path PATH $HOME/packages/mrtrix/bin" >> $HOME/modules/mrtrix/3
+  echo "prepend-path LD_LIBRARY_PATH $HOME/packages/mrtrix/lib" >> $HOME/modules/mrtrix/3
+fi
