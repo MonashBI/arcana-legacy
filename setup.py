ed89a0bc2105396862f21459b1a1b00286ef1858
Tom Close <tom.g.close@gmail.com>
2018-02-20 09:34:30 +1100

    fixed up pydicom dependency link

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=['xnat>=0.3.4',
                       'nipype>=1.0',
                       'pydicom>=1.0'],
-    dependency_links=['git+https://github.com/pydicom/pydicom.git'],
+    dependency_links=['git+https://github.com/pydicom/pydicom.git#pydicom-1.0'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Healthcare Industry",
