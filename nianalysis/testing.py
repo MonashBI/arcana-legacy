e36941904dcc0b44de487fc83819bfc60e47ee49
Tom Close <tom.g.close@gmail.com>
2018-04-16 12:14:47 +1000

    reinstated XNATError

--- nianalysis/testing.py
+++ nianalysis/testing.py
@@ -3,7 +3,7 @@ import subprocess as sp
 import shutil
 from unittest import TestCase
 import errno
-from xnat.exceptions import XnatError
+from xnat.exceptions import XNATError
 import sys
 import json
 from collections import defaultdict
@@ -331,7 +331,7 @@ class BaseMultiSubjectTestCase(BaseTestCase):
             download_all_datasets(
                 cache_dir, self.SERVER, self.xnat_session_name,
                 overwrite=False)
-        except XnatError as e:
+        except XNATError as e:
             if os.path.exists(cache_dir):
                 warnings.warn(
                     "Could not download datasets from '{}_{}' session on "
