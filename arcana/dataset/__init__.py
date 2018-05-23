3d7c4b307ea33d53efc4216d8eaad8ccdf900386
Tom Close <tom.g.close@gmail.com>
2018-05-24 09:15:26 +1000

    renamed dataset.instance to dataset.explicit

--- arcana/dataset/__init__.py
+++ arcana/dataset/__init__.py
@@ -1,4 +1,4 @@
-from .instance import Dataset, Field
+from .explicit import Dataset, Field
 from .spec import DatasetSpec, FieldSpec, BaseSpec
 from .base import BaseField, BaseDataset, BaseDatum
 from .match import DatasetMatch, FieldMatch, BaseMatch
