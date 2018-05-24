fef4628ff3b94ecd27c509813400c71b710107e0
Tom Close <tom.g.close@gmail.com>
2018-05-24 13:17:26 +1000

    updated xnat requirements

--- install/xnat.sh
+++ install/xnat.sh
@@ -2,8 +2,8 @@ DOCKER_COMPOSE_VER=1.21.2
 XNAT_VER=1.7.4
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /packages/docker-compose
-chmod +x /packages/docker-compose
+# curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VER}/docker-compose-`uname -s`-`uname -m` -o /packages/docker-compose
+# chmod +x /packages/docker-compose
 
 # Clone xnat-docker-compose
 git clone https://github.com/monashbiomedicalimaging/xnat-docker-compose /packages/xnat-docker-compose
@@ -11,7 +11,7 @@ git clone https://github.com/monashbiomedicalimaging/xnat-docker-compose /packag
 # Download XNAT WAR
 pushd /packages/xnat-docker-compose
 mkdir webapps
-wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-1.7.4.war -O webapps/xnat.war
+wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O webapps/xnat.war
 
 # Run docker-compose up
 sudo /packages/docker-compose -f docker-compose.yml up -d
