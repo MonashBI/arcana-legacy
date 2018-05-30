f19a1e476d0d5158c8a8844ccd3faa6319bc7c44
Tom Close <tom.g.close@gmail.com>
2018-05-30 13:20:00 +1000

    changed second Python 3 version to 3.3

--- install/xnat.sh
+++ install/xnat.sh
@@ -3,13 +3,15 @@ XNAT_VER=1.7.4
 mkdir -p $HOME/packages
 PKG_DIR=$HOME/packages/xnat-docker-compose
 
+sudo rm -rf $PKG_DIR
+
 if [ ! -d $PKG_DIR ]; then
   git clone https://github.com/NrgXnat/xnat-docker-compose $PKG_DIR
   pushd $PKG_DIR
   mkdir webapps
-  wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O webapps/xnat.war
+  wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O webapps/ROOT.war
   popd
 fi
 
 # Run docker-compose up
-docker-compose -f docker-compose.yml up -d
+docker-compose -f $PKG_DIR/docker-compose.yml up -d
