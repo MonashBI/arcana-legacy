1890f0e546bd5dfb95adc84debf7df6bc9e87f7f
Tom Close <tom.g.close@gmail.com>
2018-05-30 15:04:17 +1000

    touched up wait_for_xnat check

--- ci/wait_for_xnat.py
+++ ci/wait_for_xnat.py
@@ -10,22 +10,27 @@ EXPECTED_PREFIX = 'xnat-web_1    | INFO: Server startup in'
 NUM_ATTEMPTS = 10
 WAIT_TIME = 600
 
-dc_path = os.path.expanduser(
-    '~/packages/xnat-docker-compose/docker-compose.yml')
+dc_path = os.path.expanduser('~/packages/xnat-docker-compose')
 
 wait_incr = WAIT_TIME // NUM_ATTEMPTS
 
-# Check the logs to see whether the server should be up
-for i in range(NUM_ATTEMPTS):
-    tail = sp.check_output(
-        ('/usr/local/bin/docker-compose logs -f {} --tail=1 xnat-web'
-         .format(dc_path)), shell=True)
-    if tail.startswith(EXPECTED_PREFIX):
-        break
-    print("Found '{}' at tail of xnat-web log after {} seconds. "
-          "Will wait up until {} seconds before attempting to "
-          "connect".format(tail, i * wait_incr, WAIT_TIME))
-    time.sleep(wait_incr)
+orig_dir = os.getcwd()
+
+try:
+    os.chdir(dc_path)
+    # Check the logs to see whether the server should be up
+    for i in range(NUM_ATTEMPTS):
+        tail = sp.check_output(
+            ('/usr/local/bin/docker-compose logs -f --tail=1 xnat-web'),
+            shell=True)
+        if tail.startswith(EXPECTED_PREFIX):
+            break
+        print("Found '{}' at tail of xnat-web log after {} seconds. "
+              "Will wait up until {} seconds before attempting to "
+              "connect".format(tail, i * wait_incr, WAIT_TIME))
+        time.sleep(wait_incr)
+finally:
+    os.chdir(orig_dir)
 
 # Test to see whether we can connect to the XNAT server
 xnat.connect(os.environ['ARCANA_TEST_XNAT'], verify=True)
