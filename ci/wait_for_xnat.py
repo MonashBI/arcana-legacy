94f5238bb633d33c2c0a319e8f5114e0ba8423be
Tom Close <tom.g.close@gmail.com>
2018-05-30 14:57:18 +1000

    added docker-compose path to travis check

--- ci/wait_for_xnat.py
+++ ci/wait_for_xnat.py
@@ -1,6 +1,6 @@
 # Checks to see whether the XNAT host is up before continuing to run
 # the unittests
-import os
+import os.path
 import xnat
 import subprocess as sp
 import time
@@ -10,13 +10,16 @@ EXPECTED_PREFIX = 'xnat-web_1    | INFO: Server startup in'
 NUM_ATTEMPTS = 10
 WAIT_TIME = 600
 
+dc_path = os.path.expanduser(
+    '~/packages/xnat-docker-compose/docker-compose.yml')
+
 wait_incr = WAIT_TIME // NUM_ATTEMPTS
 
 # Check the logs to see whether the server should be up
 for i in range(NUM_ATTEMPTS):
     tail = sp.check_output(
-        '/usr/local/bin/docker-compose logs -f --tail=1 xnat-web',
-        shell=True)
+        ('/usr/local/bin/docker-compose logs -f {} --tail=1 xnat-web'
+         .format(dc_path)), shell=True)
     if tail.startswith(EXPECTED_PREFIX):
         break
     print("Found '{}' at tail of xnat-web log after {} seconds. "
