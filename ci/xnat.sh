7a7d622c0f8c2bfbc3b188d6524113a781483e33
Tom Close <tom.g.close@gmail.com>
2018-06-01 11:55:55 +1000

    added checkout of arcana-ci

--- ci/xnat.sh
+++ ci/xnat.sh
@@ -6,12 +6,15 @@ PKG_DIR=$HOME/packages/xnat-docker-compose
 sudo rm -rf $PKG_DIR
 
 if [ ! -d $PKG_DIR ]; then
-  git clone https://github.com/NrgXnat/xnat-docker-compose $PKG_DIR
+  git clone https://github.com/monashbiomedicalimaging/xnat-docker-compose $PKG_DIR
   pushd $PKG_DIR
+  # Checkout special branch with prefs initialised to dummy defaults
+  git checkout arcana-ci
   mkdir webapps
   wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O webapps/ROOT.war
   popd
 fi
+
 
 # Run docker-compose up
 docker-compose -f $PKG_DIR/docker-compose.yml up -d
