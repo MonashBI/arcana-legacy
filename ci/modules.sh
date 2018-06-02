6b7b60f3069f7cea0c7fdec7b21355981396cfcc
Tom Close <tom.g.close@gmail.com>
2018-06-02 11:05:21 +1000

    removed installations of mrtrix and dcm2niix

--- ci/modules.sh
+++ ci/modules.sh
@@ -1,7 +1,6 @@
 # Install environment modules to handle package versions
 
 mkdir -p $HOME/modules
-mkdir -p $HOME/packages
 mkdir -p $HOME/downloads
 
 MODULE_VER=4.1.0
@@ -22,4 +21,31 @@ if [ ! -d $HOME/packages/modules ]; then
   cp $HOME/downloads/modules-$MODULE_VER/compat/etc/global/profile.modules $HOME/modules.sh
   sed -i 's/Modules\///g' $HOME/modules.sh
   popd
+fi
+
+# Create modulefile
+if [ ! -d $HOME/modules/firstmodule ]; then
+  mkdir -p $HOME/modules/firstmodule
+  echo '#%Module1.0' >> $HOME/modules/firstmodule/0.16.2
+  echo 'proc ModulesHelp { } {' >> $HOME/modules/firstmodule/0.16.2
+  echo 'global dotversion' >> $HOME/modules/firstmodule/0.16.2
+  echo 'puts stderr "\tMRtrix 0.16.2"' >> $HOME/modules/firstmodule/0.16.2
+  echo '}' >> $HOME/modules/firstmodule/0.16.2
+  echo 'module-whatis "MRtrix 0.16.2"' >> $HOME/modules/firstmodule/0.16.2
+  echo 'conflict firstmodule' >> $HOME/modules/firstmodule/0.16.2
+  echo "prepend-path PATH $HOME/packages/firstmodule/bin" >> $HOME/modules/firstmodule/0.16.2
+  echo "prepend-path LD_LIBRARY_PATH $HOME/packages/firstmodule/lib" >> $HOME/modules/firstmodule/0.16.2
+fi
+
+
+if [ ! -d $HOME/modules/secondmodule ]; then
+  mkdir -p $HOME/modules/secondmodule
+  echo '#%Module1.0' >> $HOME/modules/secondmodule/1.0.2
+  echo 'proc ModulesHelp { } {' >> $HOME/modules/secondmodule/1.0.2
+  echo 'global dotversion' >> $HOME/modules/secondmodule/1.0.2
+  echo 'puts stderr "\tDcm2niix"' >> $HOME/modules/secondmodule/1.0.2
+  echo '}' >> $HOME/modules/secondmodule/1.0.2
+  echo 'module-whatis "Dcm2niix"' >> $HOME/modules/secondmodule/1.0.2
+  echo 'conflict secondmodule' >> $HOME/modules/secondmodule/1.0.2
+  echo "prepend-path PATH $HOME/packages/secondmodule/build/bin" >> $HOME/modules/secondmodule/1.0.2
 fi
