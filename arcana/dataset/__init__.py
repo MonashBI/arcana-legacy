772e9abf6d0c1d3e19535c04c688d19a95313125
Tom Close <tom.g.close@gmail.com>
2018-07-06 11:04:03 +1000

    added relative import for new FileFormat location

--- arcana/dataset/__init__.py
+++ arcana/dataset/__init__.py
@@ -3,3 +3,4 @@ from .collection import DatasetCollection, FieldCollection
 from .spec import DatasetSpec, FieldSpec, BaseSpec
 from .base import BaseField, BaseDataset, BaseDatasetOrField
 from .match import DatasetMatch, FieldMatch, BaseMatch
+from .file_format import FileFormat, Converter, IdentityConverter
