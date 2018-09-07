6c1ac9524706744fa5f1fda99005847b26e9d4fb
Tom Close <tom.g.close@gmail.com>
2018-09-07 14:56:34 +1000

    touched up example study

--- examples/manuscript.py
+++ examples/manuscript.py
@@ -1,25 +1,27 @@
 from arcana import (Study, StudyMetaClass, FilesetSpec, FieldSpec,
                     ParameterSpec, SwitchSpec)
 from arcana.data.file_format.standard import text_format
+from nianalysis.file_format import nifti_gz_format, dicom_format
 
 
 class ExampleStudy(Study, metaclass=StudyMetaClass):
 
     add_data_specs = [
         FilesetSpec('acquired_file1', text_format),
-        FilesetSpec('acquired_file2', text_format),
+        FilesetSpec('acquired_file2', dicom_format),
         FieldSpec('acquired_field1', int),
         FieldSpec('acquired_field2', str),
         FilesetSpec('derived_file1', text_format, 'pipeline1'),
-        FilesetSpec('derived2', text_format, 'pipeline1'),
-        FilesetSpec('derived3', text_format, 'pipeline2'),
-        FieldSpec('derived4', float, 'pipeline2'),
-        FilesetSpec('derived4', text_format, 'pipeline3'),
-        FilesetSpec('derived5', text_format, 'pipeline3',
+        FilesetSpec('derived_file2', nifti_gz_format, 'pipeline1'),
+        FilesetSpec('derived_file3', text_format, 'pipeline2'),
+        FieldSpec('derived_field4', float, 'pipeline2'),
+        FilesetSpec('derived_file4', dicom_format, 'pipeline3'),
+        FilesetSpec('derived_file5', nifti_gz_format, 'pipeline3',
                     frequency='per_subject'),
-        FilesetSpec('derived6', text_format, 'pipeline4',
+        FilesetSpec('derived_file6', text_format, 'pipeline4',
                     frequency='per_visit'),
-        FieldSpec('derived7', int, 'pipeline4', frequency='per_study')]
+        FieldSpec('derived_field7', int, 'pipeline4',
+                  frequency='per_study')]
 
     add_parameter_specs = [
         ParameterSpec('parameter1', False),
