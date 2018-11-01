90977fe540eb63ff99143f6d0784b92290eb5b79
Tom Close <tom.g.close@gmail.com>
2018-11-01 21:50:35 +1100

    updated memory and nthreads to mem_gb and n_procs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ PACKAGE_NAME = 'arcana'
 # Get version from module inside package
 sys.path.insert(0, os.path.join(os.path.dirname(__file__),
                                 PACKAGE_NAME))
-from version_ import __version__  # @UnresolvedImport @IgnorePep8
+from pkg_info import __version__  # @UnresolvedImport @IgnorePep8
 sys.path.pop(0)
 
 
