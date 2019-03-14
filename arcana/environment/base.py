41434989de968dca455720a6ffd4f517002a6c8e
Tom Close <tom.g.close@gmail.com>
2019-03-14 15:54:14 +1100

    added debugging print line to travis config

--- arcana/environment/base.py
+++ arcana/environment/base.py
@@ -47,11 +47,7 @@ class NodeMixin(object):
     def _run_command(self, *args, **kwargs):
         # Detect run time and compare against specified wall_time
         start_time = time.time()
-        try:
-            result = self.nipype_cls._run_command(self, *args, **kwargs)
-        except TypeError as e:
-            print(result)
-            raise e
+        result = self.nipype_cls._run_command(self, *args, **kwargs)
         end_time = time.time()
         run_time = (end_time - start_time) // 60
         if run_time > self._wall_time:
