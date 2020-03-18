cc1ab5ae0b8568aab2ca91ae0eb1391d8600efca
Tom Close <tom.g.close@gmail.com>
2020-03-18 14:34:52 +1100

    switched branch to master from arcana-ci

--- ci/xnat.sh
+++ ci/xnat.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -d $XNAT_DIR ]; then
-	mkdir -p $XNAT_DIR
-	
-	# Clone the docker compose script
-	git clone https://github.com/MonashBI/xnat-docker-compose $XNAT_DIR
-	
-	pushd $XNAT_DIR
-	# Checkout special branch with prefs initialised to dummy defaults
-	git checkout arcana-ci
-	
-	# Download the XNAT WAR file and copy to webapps directory
-	if [ ! -f $HOME/downloads/xnat.war ]; then
-	  wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O $HOME/downloads/xnat-${XNAT_VER}.war
-	fi
-	mkdir webapps
-	cp $HOME/downloads/xnat-${XNAT_VER}.war webapps/ROOT.war
+mkdir -p $XNAT_DIR
 
-	popd
+# Clone the docker compose script
+git clone https://github.com/MonashBI/xnat-docker-compose $XNAT_DIR
+
+pushd $XNAT_DIR
+# Checkout special branch with prefs initialised to dummy defaults
+# git checkout arcana-ci
+
+# Download the XNAT WAR file and copy to webapps directory
+if [ ! -f $HOME/downloads/xnat.war ]; then
+	wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O $HOME/downloads/xnat-${XNAT_VER}.war
 fi
+mkdir webapps
+cp $HOME/downloads/xnat-${XNAT_VER}.war webapps/ROOT.war
+
+popd
