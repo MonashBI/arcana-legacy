b3b949c8c69349b6688de714001c9df9b75c950e
Tom Close <tom.g.close@gmail.com>
2020-03-18 15:10:14 +1100

    fixed up xnat download location in CI

--- ci/xnat.sh
+++ ci/xnat.sh
@@ -12,7 +12,7 @@ pushd $XNAT_DIR
 
 # Download the XNAT WAR file and copy to webapps directory
 if [ ! -f $HOME/downloads/xnat.war ]; then
-	wget --no-cookies https://bintray.com/nrgxnat/applications/download_file?file_path=xnat-web-${XNAT_VER}.war -O $HOME/downloads/xnat-${XNAT_VER}.war
+	wget --no-cookies https://api.bitbucket.org/2.0/repositories/xnatdev/xnat-web/downloads/xnat-web-${XNAT_VER}.war -O $HOME/downloads/xnat-${XNAT_VER}.war
 fi
 mkdir webapps
 cp $HOME/downloads/xnat-${XNAT_VER}.war webapps/ROOT.war
